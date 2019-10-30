91a68c267095794d6bbe297ab20f11fda9175019
Alumno <Alumno@PC30>
2019-10-30 11:41:37 -0600

    Vectores

--- Vectores/Vectores.cpp
+++ Vectores/Vectores.cpp
@@ -8,11 +8,15 @@ int main()
 {
 	int numero[5];
 	int suma;
-	numero[0] = 200;
-	numero[1] = 150;
-	numero[2] = 100;
-	numero[3] = -50;
-	numero[4] = 300;
+	
+	for (int i = 0; i <= 5; i++) 
+	{
+		cout << "Introduce un numero: ";
+		cin >> numero[i];
+		suma = +numero[1];
+
+	}
+
 	suma = numero[0] + numero[1] + numero[3] + numero[4];
 	cout << "La suma es " << suma;
 	return 0;
